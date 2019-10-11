6ad37f77d7d2464239033eedf90d32e92194c459
Mykhail1387 <melnychukmykhailo87@gmail.com>
2019-10-11 17:03:40 +0300

    HW_all tasks, prettier

--- js/task-2.js
+++ js/task-2.js
@@ -5,7 +5,7 @@ const ordered = 50;
 
 const isEnough = ordered >= 1 && ordered <= 100;
 
-if(isEnough) {
+if (isEnough) {
     console.log('Заказ оформлен, с вами свяжется менеджер');
 } else {
     console.log('На складе недостаточно товаров!');
